d1915073f360f6e195218c43fbbec06458778307
Denis <piskarew.den@yandex.ru>
2023-09-21 23:10:43 +0300

    load

---
+++ ClinicApp/HelperClasses/AgeConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ClinicApp.HelperClasses
+{
+    public class AgeConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values[0] is DateTime dateOfBirth)
+            {
+                int age = DateTime.Today.Year - dateOfBirth.Year;
+                if (DateTime.Today < dateOfBirth.AddYears(age))
+                {
+                    age--;
+                }
+                return age.ToString();
+            }
+            return null;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
